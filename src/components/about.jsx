036ae92f9261aa036085bd9a45fd2c7837a8122e
EilonV <73593619+EilonV@users.noreply.github.com>
2023-08-16 22:56:32 +0300

    styled portfolio grid, responsive

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,17 +3,14 @@ import img from '../../src/assets/images/spray.jpg'
 export const About = () => {
     return <section id='about' className="about flex">
         <h1 className="flex column">
-            <div className="about-header-blur">
-                ALL ABOUT
-            </div>
-            {/* <span>ME</span  > */}
+            <h2>ALL ABOUT</h2>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, ut? Debitis sint nemo facilis quo placeat aliquid, voluptas, nulla error non ipsum omnis vitae consequatur recusandae. Enim nulla voluptas fugit!
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus quam maiores possimus ab dicta neque velit. Cumque nobis soluta reiciendis neque nesciunt possimus autem laboriosam necessitatibus tenetur? Deserunt, sed quia?</p>
         </h1>
         <div className='flex'>
             <div className="flex column align-center justify-center">
-                <h1>ME</h1>
+                <h2>ME</h2>
                 <img src={img} alt="gaya shayo pfp" />
             </div>
         </div>
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,7 +1,7 @@
 export const Portfolio = () => {
     return <section id='portfolio' className="portfolio-wrapper full">
-        <div className="portfolio main-layout">
-            <h1>PORTFOLIO</h1>
+        <div className="portfolio main-layout justify-center">
+            <h2>PORTFOLIO</h2>
             <div className="gallery">
                 <div className="item">x</div>
                 <div className="item">x</div>
