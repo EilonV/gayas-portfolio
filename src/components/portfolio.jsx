6b35868df7e576aab4d29e644f703da91142cc3e
EilonV <73593619+EilonV@users.noreply.github.com>
2023-08-16 14:56:55 +0300

    added respo portfolio grid for projects

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,6 +2,18 @@ export const Portfolio = () => {
     return <section id='portfolio' className="portfolio-wrapper full">
         <div className="portfolio main-layout">
             <h1>PORTFOLIO</h1>
+            <div className="gallery">
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+                <div className="item">x</div>
+            </div>
         </div>
     </section>
 }
